d53d9c4997ae4f7a0699086ef975a22ef0032eeb
s000ik <s000ik@users.noreply.github.com>
2024-06-19 18:01:00 +0530

    Vercel optimizations

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+// next.config.js
 module.exports = {
-  reactStrictMode: true
-};
+  // Other Next.js config options
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  distDir: 'build', // Optional: Custom build output directory
+  experimental: {
+    appDir: true, // Enable app directory if using it
+  }
+}
